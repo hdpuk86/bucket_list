541ce9d8cc311a6071861d6e10c85150007ccb1f
Jamie Klein <jamiesklein@gmail.com>
2017-12-13 11:49:52 +0000

    Moves countrySearch to app.js.

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,21 @@
-window.addEventListener("load", function(){
-	alert("Loaded!");
+const onCountriesRequestLoad = function(event) {
+  const resultString = event.target.responseText;
+  let searchResults = JSON.parse(resultString);
+  console.log(searchResults);
+};
+
+const makeCountriesRequest = function(searchString) {
+  const url = `https://restcountries.eu/rest/v2/name/${searchString}`;
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', onCountriesRequestLoad);
+  request.send();
+};
+
+const countrySearch = function(searchString) {
+  makeCountriesRequest(searchString);
+};
+
+window.addEventListener('load', function() {
+  countrySearch('united');
 });
