11ff2fcf055b6254e4b1910e166831c7223262c9
Jamie Klein <jamiesklein@gmail.com>
2017-12-13 11:07:52 +0000

    Adds makeRequest function.

--- client/src/countrySearch.js
+++ client/src/countrySearch.js
@@ -1,5 +1,15 @@
 const CountrySearch = function(searchString) {
-  
+  let result;
+
+  const makeRequest = function() {
+    const url = `https://restcountries.eu/rest/v2/name/${searchString}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', onRequestLoad);
+    request.send();
+  };
+
+  makeRequest();
 };
 
 module.exports = CountrySearch;
