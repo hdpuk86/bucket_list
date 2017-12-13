d147447bc67458736b55d7e82247eda210677a95
Jamie Klein <jamiesklein@gmail.com>
2017-12-13 12:47:16 +0000

    Moves countrySearch into a file and passes in a callback for cleanliness.

---
+++ client/src/countrySearch.js
@@ -0,0 +1,19 @@
+const CountrySearch = function(searchString, callback) {
+  const onCountriesRequestLoad = function(event) {
+    const resultString = event.target.responseText;
+    let searchResults = JSON.parse(resultString);
+    callback(searchResults);
+  };
+
+  const makeCountriesRequest = function() {
+    const url = `https://restcountries.eu/rest/v2/name/${searchString}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', onCountriesRequestLoad);
+    request.send();
+  };
+
+  makeCountriesRequest();
+};
+
+module.exports = CountrySearch;
