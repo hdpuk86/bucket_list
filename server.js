3035fed91ebdfd55019e4b68558c42691f6058ff
Findsay <fiona.e.lindsay@gmail.com>
2017-12-13 12:19:03 +0000

    Adding server js cchangers

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + "/client/build/index.html");
 });
 
+app.get("/bucket_list", function(req, res){
+	res.json({name: "test"});
+});
+
 app.listen(3000, function(){
 	console.log("Listening");
 });
