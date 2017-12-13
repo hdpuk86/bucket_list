d592043fc98636e3ea7edcd8f7992cb316acf600
Findsay <fiona.e.lindsay@gmail.com>
2017-12-13 11:37:50 +0000

    Gave up on trying to do this in seperate file and reveretd back to just putting the request in the server.js file

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,26 @@
+var makeBucketListRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+};
+
+var bucketListRequestComplete = function(){
+  if(this.status !== 200){
+    return;
+  }
+  var jsonString = this.responseText;
+  var bucketList = JSON.parse(jsonString);
+};
+
+var loadBucketList = function(){
+  var url = "/bucket_list";
+  makeRequest(url, requestComplete);
+};
+
 window.addEventListener("load", function(){
 	alert("Loaded!");
+	loadBucketList();
+
+
 });
