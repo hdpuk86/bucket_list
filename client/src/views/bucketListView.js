341e8debe07be6e1e1ab5228dbd782a097367737
Findsay <fiona.e.lindsay@gmail.com>
2017-12-13 13:50:47 +0000

    refactored buketlistv iew

--- client/src/views/bucketListView.js
+++ client/src/views/bucketListView.js
@@ -4,21 +4,27 @@ var BucketListView = function(bucketlist){
 
 BucketListView.prototype = {
   render: function(bucketlist){
-    bucketlist.forEach(country){
+    var div = document.getElementById('bucket-list');
+    div.innerText = "";
+    bucketlist.forEach(function(country{
       var li = document.createElement('li');
       li.innerText = country.name;
+
       var deletebtn = document.createElement('button');
       deletebtn.innerText = "Remove";
+
       var visited = document.createElement('input');
       visited.setAttribute("type", "checkbox");
-      var div = document.getElementById('bucket-list');
+
       var ul = document.createElement('ul');
-      deletebtn.appendChild(visited);
+      li.appendChild(visited);
       li.appendChild(deletebtn);
       ul.appendChild(li);
       div.appendChild(ul);
-    }
+    })
+
   }
+
 };
 
 module.exports = BucketListView;
